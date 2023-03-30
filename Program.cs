6298da6d7158964fc33d15a0eedf20fc790b9f16
Miraj <mirajiik@yandex.ru>
2023-03-31 04:11:29 +0500

    reworking algorithm and renaming according to GOST 34.10.2018

--- Program.cs
+++ Program.cs
@@ -6,55 +6,59 @@ public class Program
     static readonly int a = 0, b = 7; //Curve secp256k1 
     static readonly BigInteger p = BigInteger.Parse("0 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff fffffffe fffffc2f".Replace(" ", ""),
             System.Globalization.NumberStyles.HexNumber);
-    static readonly BigInteger n = BigInteger.Parse("0 ffffffff ffffffff ffffffff fffffffe baaedce6 af48a03b bfd25e8c d0364141".Replace(" ", ""),
+    static readonly BigInteger q = BigInteger.Parse("0 ffffffff ffffffff ffffffff fffffffe baaedce6 af48a03b bfd25e8c d0364141".Replace(" ", ""),
         System.Globalization.NumberStyles.HexNumber);
     static readonly Random rnd = new Random();
-    static readonly BigInteger Gx = BigInteger.Parse("0 79be667e f9dcbbac 55a06295 ce870b07 029bfcdb 2dce28d9 59f2815b 16f81798".Replace(" ", ""),
+    static readonly BigInteger Px = BigInteger.Parse("0 79be667e f9dcbbac 55a06295 ce870b07 029bfcdb 2dce28d9 59f2815b 16f81798".Replace(" ", ""),
             System.Globalization.NumberStyles.HexNumber);
-    static readonly BigInteger Gy = BigInteger.Parse("0 483ada77 26a3c465 5da4fbfc 0e1108a8 fd17b448 a6855419 9c47d08f fb10d4b8".Replace(" ", ""),
+    static readonly BigInteger Py = BigInteger.Parse("0 483ada77 26a3c465 5da4fbfc 0e1108a8 fd17b448 a6855419 9c47d08f fb10d4b8".Replace(" ", ""),
         System.Globalization.NumberStyles.HexNumber);
     private static void Main(string[] args)
     {
-        Console.WriteLine($"Curve secp256k1\na = {a}, b = {b}\np = {p}\nn = {n}\nGx = {Gx}\nGy = {Gy}\n");
+        Console.WriteLine($"Curve secp256k1\na = {a}, b = {b}\np = {p}\nq = {q}\nPx = {Px}\nPy = {Py}\n");
 
         BigInteger dA; //Закрытый ключ
-        BigInteger publicX, publicY; //Точка, открытый ключ
-        (dA, (publicX, publicY)) = KeyGen(); // Генерация открытого и закрытого ключа
-        Console.WriteLine($"publicX = {publicX}\npublicY = {publicY}");
+        BigInteger Qx, Qy; //Точка, открытый ключ
+        (dA, (Qx, Qy)) = KeyGen(); // Генерация открытого и закрытого ключа
+        Console.WriteLine($"Qx = {Qx}\nQy = {Qy}");
 
         Console.WriteLine("Enter message:");
         string message = Console.ReadLine();
 
         (BigInteger r, BigInteger s) = SignMessage(message, dA);// Подпись сообщения
 
-        Console.WriteLine($"Verification = {VerificationSignature(r, s, publicX, publicY, message)}"); //Проверка подписи
+        Console.WriteLine($"Verification = {VerificationSignature(r, s, Qx, Qy, message)}"); //Проверка подписи
     }
 
-    public static bool VerificationSignature(BigInteger r, BigInteger s, BigInteger px, BigInteger py, string message)
+    public static bool VerificationSignature(BigInteger r, BigInteger s, BigInteger Qx, BigInteger Qy, string message)
     {
         message = "Hello!";
         byte[] hash = new byte[33]; //Создаем массив под хэш на 1 байт больше, потому что в BigInteger'e последний байт отводится под знак числа
                                     //(положительный < 128, отрицательный >= 128)
         Streebog.HashFunc(Encoding.Default.GetBytes(message), Streebog.OutSize.bit256, Streebog.TypeInput.Chars).Reverse().ToArray().CopyTo(hash, 0);
-        BigInteger z = new BigInteger(hash);
-#if DEBUG
-        Console.WriteLine($"z = {z}");
-#endif
-        BigInteger w = inverse(s, n);
-        BigInteger u1 = z * w % n;
-        BigInteger u2 = r * w % n;
-        (BigInteger resultX, BigInteger resultY) = AddPoints(Mult(Gx, Gy, u1), Mult(px, py, u2));
-        return r % n == resultX % n;
+        BigInteger h = new BigInteger(hash);
+        BigInteger e = h % q;
+        if (e == 0)
+            e = 1;
+        BigInteger v = inverse(e, q);
+        BigInteger z1 = s * v % q;
+        BigInteger z2 = -1 * r * v % q + q;
+        (BigInteger Cx, BigInteger Cy) = AddPoints(Mult(Px, Py, z1), Mult(Qx, Qy, z2));
+        BigInteger R = Cx % q;
+        return r == R;
     }
 
     public static (BigInteger, BigInteger) SignMessage(string message, BigInteger dA)
     {
-        BigInteger k, px, py; //Закрытый ключ
+        BigInteger k, Cx, Cy; //Закрытый ключ
         byte[] hash = new byte[33]; //Создаем массив под хэш на 1 байт больше, потому что в BigInteger'e последний байт отводится под знак числа
                                     //(положительный < 128, отрицательный >= 128)
         Streebog.HashFunc(Encoding.Default.GetBytes(message), Streebog.OutSize.bit256, Streebog.TypeInput.Chars).Reverse().ToArray().CopyTo(hash, 0);
-        BigInteger z = new BigInteger(hash);
-        Console.WriteLine($"Hash = {BitConverter.ToString(hash.SkipLast(1).ToArray())}\nz = {z}");
+        BigInteger h = new BigInteger(hash);
+        Console.WriteLine($"Hash = {BitConverter.ToString(hash.SkipLast(1).ToArray())}\nh = {h}");
+        BigInteger e = h % q;
+        if (e == 0)
+            e = 1;
 
         BigInteger r;
         BigInteger s;
@@ -62,12 +66,13 @@ public class Program
         {
             do
             {
-                k = rnd.Next(n);
-                (px, py) = Mult(Gx, Gy, k);
-                r = px % n;
-                Console.WriteLine($"k = {k}\nPx = {px}\nPy = {py}\nr = {r}");
+                k = rnd.Next(q);
+                (Cx, Cy) = Mult(Px, Py, k);
+                r = Cx % q;
+                Console.WriteLine($"k = {k}\nPx = {Cx}\nPy = {Cy}\nr = {r}");
             } while (r == 0);
-            s = inverse(k, n) * (z + r * dA) % n;
+            //s = inverse(k, q) * (z + r * dA) % q;
+            s = (r * dA + k * e) % q;
             Console.WriteLine($"s = {s}\n");
         } while (s == 0);
 
@@ -76,8 +81,8 @@ public class Program
 
     public static (BigInteger, (BigInteger, BigInteger)) KeyGen()
     {
-        var dA = rnd.Next(n);
-        return (dA, Mult(Gx, Gy, dA));
+        var dA = rnd.Next(q);
+        return (dA, Mult(Px, Py, dA));
     }
 
     public static (BigInteger, BigInteger) Mult(BigInteger x, BigInteger y, BigInteger k)
